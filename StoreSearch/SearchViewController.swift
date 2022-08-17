eb093c7e19de7b3f2ad5f26869227b557461879b
Gospodi <gospodijazzet@gmail.com>
2022-08-17 14:49:49 +0300

    соединеняем контроллер представления поиска с другими объектами

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -25,3 +25,20 @@ extension SearchViewController: UISearchBarDelegate {
   }
 }
 
+// MARK: - Table View Delegate
+extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
+  func tableView(
+    _ tableView: UITableView,
+    numberOfRowsInSection section: Int
+  ) -> Int {
+    return 0
+  }
+
+  func tableView(
+    _ tableView: UITableView,
+    cellForRowAt indexPath: IndexPath
+  ) -> UITableViewCell {
+    return UITableViewCell()
+  }
+}
+
