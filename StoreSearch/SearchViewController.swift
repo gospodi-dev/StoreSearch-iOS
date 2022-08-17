16b6797f9fe09a78027cd5d55160218764deafbc
Gospodi <gospodijazzet@gmail.com>
2022-08-17 14:24:42 +0300

    добавлена привязка ui компонентов к коду

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
