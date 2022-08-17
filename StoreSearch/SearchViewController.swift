e6e9fc15448f6817710d1797573a700792c0a0b8
Gospodi <gospodijazzet@gmail.com>
2022-08-17 14:56:33 +0300

    изменен searchBarSearchButtonClicked(_:) метод для работы с тестовыми данными

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -22,25 +22,33 @@ class SearchViewController: UIViewController {
 
 // MARK: - Search Bar Delegate
 extension SearchViewController: UISearchBarDelegate {
-  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-    print("The search text is: '\(searchBar.text!)'")
-  }
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchResults = []
+        for i in 0...2 {
+            searchResults.append(
+                String(
+                    format: "Fake Result %d for '%@'", i, searchBar.text!
+                )
+            )
+        }
+        tableView.reloadData()
+    }
 }
 
 // MARK: - Table View Delegate
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
-  func tableView(
-    _ tableView: UITableView,
-    numberOfRowsInSection section: Int
-  ) -> Int {
-    return 0
-  }
+    func tableView(
+        _ tableView: UITableView,
+        numberOfRowsInSection section: Int
+    ) -> Int {
+        return 0
+    }
 
-  func tableView(
-    _ tableView: UITableView,
-    cellForRowAt indexPath: IndexPath
-  ) -> UITableViewCell {
-    return UITableViewCell()
-  }
+    func tableView(
+        _ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+        return UITableViewCell()
+    }
 }
 
