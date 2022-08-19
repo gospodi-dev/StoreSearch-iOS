7e9064a940258e504048afa734fe19d6ed59b73f
Gospodi <gospodijazzet@gmail.com>
2022-08-19 15:43:41 +0300

    создан собственный подкласс SearchResultCell для вывода модуля

---
+++ StoreSearch/SearchResultCell.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResultCellTableViewCell.swift
+//  StoreSearch
+//
+//  Created by Gospodi on 19.08.2022.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -65,12 +65,9 @@ extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     ) -> UITableViewCell {
         let cellIdentifier = "SearchResultCell"
         
-        var cell: UITableViewCell! = tableView.dequeueReusableCell(
-          withIdentifier: cellIdentifier)
-        if cell == nil {
-          cell = UITableViewCell(
-            style: .subtitle, reuseIdentifier: cellIdentifier)
-        }
+        let cell = tableView.dequeueReusableCell(
+          withIdentifier: cellIdentifier,          
+          for: indexPath)
         if searchResults.count == 0 {
             cell.textLabel!.text = "(Nothing found)"
             cell.detailTextLabel!.text = ""
